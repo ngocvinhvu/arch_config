9e89c1a105a079a219c2060ea298eda61d81dd7b
Nguyen Ngoc Duy <hotboyarsenal@gmail.com>
2020-05-14 03:38:55 +0700

    update

--- dwm_gridmod/config.h
+++ dwm_gridmod/config.h
@@ -42,9 +42,11 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+#include "grid.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
+        { "HHH",      grid },
 	// { "[M]",      monocle },
 	// { "><>",      NULL },    /* no layout function means floating behavior */
 };
@@ -84,6 +86,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[1]} },
 	// { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
 	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
 	// { MODKEY,                       XK_space,  setlayout,      {0} },
