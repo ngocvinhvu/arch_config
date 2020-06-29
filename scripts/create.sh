dff5a20df38679d2066195f0fab9afd9de0e85d5
Nguyen Ngoc Duy <hotboyarsenal@gmail.com>
2020-06-29 13:37:22 +0700

    update

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,1 +1,1 @@
-for ((i=$1; i<=$2; i++)); do echo "Do you want to create $3$i?"; yes ""| sudo mysql -u root -p -Bse "create database $3$i;";done;
+for ((i=$2; i<=$3; i++)); do echo "Do you want to create $1$i?"; sudo mysql -u root -p -Bse "create database $1$i;";done;
--- scripts/drop.sh
+++ scripts/drop.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-for ((i=$1; i<=$2; i++)); do echo "Do you want to drop $3$i"; sudo mysql -u root -p -Bse "drop database $3$i;"; done;
+for ((i=$2; i<=$3; i++)); do echo "Do you want to drop $1$i"; sudo mysql -u root -p -Bse "drop database $1$i;"; done;
