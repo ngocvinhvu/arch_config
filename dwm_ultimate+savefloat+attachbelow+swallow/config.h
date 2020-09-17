932bc737a8e787aae742f6531422114d6019cadb
abc@xyz.com <abc@xyz.com>
2020-09-18 02:52:26 +0700

    update

--- dwm_ultimate+savefloat+attachbelow+swallow/config.h
+++ dwm_ultimate+savefloat+attachbelow+swallow/config.h
@@ -17,7 +17,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	// [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	// [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeNorm] = { "#ededcc", "#000000", col_gray2 },
+	[SchemeNorm] = { "#18B218", "#000000", col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  "#00A5FF"}, // neon blue
 };
 
