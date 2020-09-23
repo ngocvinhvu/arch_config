992a37fa3e80511947646dc333e7280e40a06d9e
abc@xyz.com <abc@xyz.com>
2020-09-24 03:51:17 +0700

    update

--- dwm/config.h
+++ dwm/config.h
@@ -59,11 +59,11 @@ static int attachbelow = 1;    /* 1 means attach after the currently active wind
 #include "grid.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "HHH",      grid},    
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
+	{ "HHH",      grid},    
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 	// { "[M]",      monocle },
 };
 
@@ -81,7 +81,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL};
+static const char *termcmd[]  = { "st", "tmux"};
 
 #include <X11/XF86keysym.h>
 #include "movestack.c"
