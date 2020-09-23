0068b85a9e39c58d1cea082578889a1be76d2a9a
abc@xyz.com <abc@xyz.com>
2020-09-24 01:19:36 +0700

    update

--- dwm/config.h
+++ dwm/config.h
@@ -62,6 +62,8 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "HHH",      grid},    
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 	// { "[M]",      monocle },
 };
 
@@ -103,6 +105,8 @@ static Key keys[] = {
 	{ MODKEY,               XK_t,          	setlayout,      	{.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,     XK_t,          	setlayout,      	{.v = &layouts[1]} },
 	{ MODKEY,               XK_u,          	setlayout,      	{.v = &layouts[2]} },
+	{ MODKEY,               XK_y,          	setlayout,      	{.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,     XK_y,          	setlayout,      	{.v = &layouts[4]} },
  // { MODKEY,               XK_space,      	setlayout,      	{0} },
 	{ MODKEY|ShiftMask,     XK_space,      	togglefloating, 	{0} },
 	{ MODKEY,               XK_f,          	togglefullscr,  	{0} },
