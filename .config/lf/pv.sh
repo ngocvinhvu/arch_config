68912319128cd3fac3655e825041e85302abcfb8
abc@xyz.com <abc@xyz.com>
2020-09-25 05:15:10 +0700

    update

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -6,7 +6,7 @@ case "$1" in
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
-	*.png|*.jpg) exiftool "$1" && exit 5; exit 1;;
-	*.mkv|*.mp4|*.mp3|*.flac|*.webm) mediainfo "$1" && exit 5; exit 1;;
+	*.png|*.jpg) exiftool "$1" ;;
+	*.mkv|*.mp4|*.mp3|*.flac|*.webm) mediainfo "$1" ;;
     *) highlight -O ansi "$1" || cat "$1";;
 esac
