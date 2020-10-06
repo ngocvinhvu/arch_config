35aa5934a29c24a5bfe5b795589170f39f7b77f1
abc.xyz <abc.xyz>
2020-10-06 09:56:33 +0700

    update

--- dwm/config.h
+++ dwm/config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "mono:size=8", "Ionicons:size=8" };
 static const char dmenufont[]       = "mono:size=10";
 static const char col_gray1[]       = "#222222";
