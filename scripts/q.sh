b20ba954408ac9e022cd84c6fe75cb8f24b6c68c
abc.xyz <abc.xyz>
2020-11-14 20:48:38 +0700

    update

---
+++ scripts/q.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+url=$({
+    cat ~/.config/qutebrowser/bookmarks/urls
+    awk '{ tmp = $1; $1 = $2; $2 = tmp; print $0 }' ~/.config/qutebrowser/quickmarks
+    sqlite3 -separator ' ' ~/.local/share/qutebrowser/history.sqlite 'SELECT url, title FROM CompletionHistory'
+} | dmenu -l 10 -p "qute history search $* " | cut -d ' ' -f 1)
+
+[[ -n $url ]] && $BROWSER $url || exit 0
