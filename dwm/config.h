6d086fb7e4a13e9fa3709908c12b223d1e2f179b
Duy <you@example.com>
2021-03-30 16:08:24 +0700

    update

--- dwm/config.h
+++ dwm/config.h
@@ -39,7 +39,7 @@ static const Rule rules[] = {
         { "Zathura",       NULL,       NULL,            1 << 8,       0,           0,           1,         -1 },
         { "Evince",        NULL,       NULL,            1 << 8,       0,           0,           1,         -1 },
         { "FBReader",      NULL,       NULL,            1 << 8,       0,           0,           1,         -1 },
-        { "firefox",       NULL,       NULL,            1 << 7,       0,           0,           1,         -1 },
+        { "firefox",       NULL,       NULL,            1 << 2,       0,           0,           1,         -1 },
         { "chromium",      NULL,       NULL,            1 << 1,       0,           0,           1,         -1 },
         { "St",            NULL,       NULL,            0,            0,           1,           1,         -1 },
         { "Alacritty",     NULL,       NULL,            0,            0,           1,           1,         -1 },
