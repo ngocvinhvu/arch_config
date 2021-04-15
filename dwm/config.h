25e8bf0f0058b3216f4f84560a6693c260ba9c5a
abc.xyz <abc.xyz>
2021-04-16 02:01:44 +0700

    update

--- dwm/config.h
+++ dwm/config.h
@@ -95,6 +95,8 @@ static const char *termcmd[]  = { "st", NULL};
 static Key keys[] = {
 	/* modifier             key				function			argument */
 	{ MODKEY,               XK_d,          	spawn,          	{.v = dmenucmd } },
+	{ Mod4Mask|ControlMask, XK_c,          	spawn,          	SHCMD("clipboard_manager.sh grab") },
+	{ Mod4Mask|ControlMask, XK_v,          	spawn,          	SHCMD("clipboard_manager.sh select2paste") },
 	{ MODKEY,               XK_Return,     	spawn,          	{.v = termcmd } },
 	{ MODKEY|ShiftMask,		XK_Return,		spawn,				SHCMD("st -e tmux") },
 	{ MODKEY,               XK_b,          	togglebar,      	{0} },
