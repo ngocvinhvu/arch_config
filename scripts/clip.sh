53dbedd0a3601cfa8c43ca574df749af0ac70d1f
Duy <you@example.com>
2021-04-22 08:11:29 +0700

    update

--- scripts/clip.sh
+++ scripts/clip.sh
@@ -5,20 +5,18 @@ size=10
 if [ "$1" == "copy" ]; then
     sel_clip=$(xsel -o)
     sel_file=$(echo -n "$sel_clip"|tr '\n' '\034')
+	sed "/^$sel_file$/d" -i $file
+	cut=$(head -n $(($size-1)) $file)
+	echo "$sel_file" > $file
+	echo -n "$cut" >> $file
 fi
-touch $file
+# touch $file
 if [ "$1" == "paste" ]; then
     shift
-    sel_file=$(awk '!x[$0]++' $file | uniq | dmenu -l 10 ${1+"$@"})
+    sel_file=$(awk '!x[$0]++' $file | uniq | dmenu -l 10 ${1+"$@"}) 
+	echo $sel_file | xclip -f -sel clip > /dev/null
 fi
 if [ "$1" == "clear" ]; then
 	echo -n > $file
 fi
 [ "$sel_clip" == "" ] && exit 1
-sed "/^$sel_file$/d" -i $file
-cut=$(head -n $(($size-1)) $file)
-echo "$sel_file" > $file
-echo -n "$cut" >> $file
-echo -n "$sel_clip" | xsel -p -i
-echo -n "$sel_clip" | xsel -b -i
-exit 0
